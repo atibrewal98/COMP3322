57b5667de34c52bb45b4b9b75c9220de10e89d5e
atibrewal98 <ankit@siplnet.com>
2020-03-16 23:07:12 +0530

    Task 2

--- Lab-2/Workshop2_2020/queryEntries_WS2.php
+++ Lab-2/Workshop2_2020/queryEntries_WS2.php
@@ -1,6 +1,9 @@
 <?php
-	
-	$conn=mysqli_connect('sophia.cs.hku.hk', 'user-name', 'password', ‘database name’) or die ('Error! '.mysqli_connect_error($conn));
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+	header('Access-Control-Allow-Headers: token, Content-Type');
+	header('Access-Control-Max-Age: 1728000');
+	$conn=mysqli_connect('sophia.cs.hku.hk', 'tibrewal', 'KLIipPTB', 'tibrewal') or die ('Error! '.mysqli_connect_error($conn));
 
 	
 	if($_POST['show'] =='add') {
--- Lab-2/Workshop2_2020/updateState_WS2.php
+++ Lab-2/Workshop2_2020/updateState_WS2.php
@@ -1,5 +1,6 @@
 <?php
-	$conn=mysqli_connect('sophia.cs.hku.hk', 'user-name', 'password', ‘database name’) or die ('Error! '.mysqli_connect_error($conn));
+	header("Access-Control-Allow-Origin: *");
+	$conn=mysqli_connect('sophia.cs.hku.hk', 'tibrewal', 'KLIipPTB', ‘tibrewal’) or die ('Error! '.mysqli_connect_error($conn));
 
 	// Implement the code here.
 
