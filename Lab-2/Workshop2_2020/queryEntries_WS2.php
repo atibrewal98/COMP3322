3768dc773059d98146be89d3f89d3df4400f1fd8
atibrewal98 <ankit@siplnet.com>
2020-03-18 18:46:58 +0530

    Add Student Initial Implementation

--- Lab-2/Workshop2_2020/queryEntries_WS2.php
+++ Lab-2/Workshop2_2020/queryEntries_WS2.php
@@ -3,16 +3,21 @@
 	header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 	header('Access-Control-Allow-Headers: token, Content-Type');
 	header('Access-Control-Max-Age: 1728000');
-	$conn=mysqli_connect('sophia.cs.hku.hk', 'tibrewal', 'KLIipPTB', 'tibrewal') or die ('Error! '.mysqli_connect_error($conn));
+	$conn = mysqli_connect('sophia.cs.hku.hk', 'tibrewal', 'KLIipPTB', 'tibrewal') or die ('Error! '.mysqli_connect_error($conn));
 
-	
 	if($_POST['show'] =='add') {
+		$query = 'Insert Into attendancelist Values(\''.$_POST['name'].'\', \''.$_POST['major'].'\', \''.$_POST['course'].'\', \''.$_POST['cdate'].'\', \''.$_POST['attd'].'\')';
+		$result = mysqli_query($conn, $query) or die ('Failed to query '.mysqli_error($conn));
+		$query = 'select * from attendancelist';
+		$result = mysqli_query($conn, $query) or die ('Failed to query '.mysqli_error($conn));
 		// add code here
 		while($row = mysqli_fetch_array($result)) {
-			print "<div id=".$row['id'].">";
-			print "<span>"."</span>";
-			print "</div>";
-		}
+		    print "<div id=".$row['id'].">";
+			print "<span>".$row['attendOrNot']."</h3>";
+			print "<h3>".$row['studentname']." (".$row['major'].")</h3>";
+			print "<h5>(".$row['course'].") on ".$row['coursedate']."</h5>";
+		    print "</div>";
+	    }
 	} elseif ($_POST['show'] == 'all') {
 		$query = 'select * from attendancelist';
 		$result = mysqli_query($conn, $query) or die ('Failed to query '.mysqli_error($conn));
@@ -23,7 +28,6 @@
 			print "<h5>(".$row['course'].") on ".$row['coursedate']."</h5>";
 		    print "</div>";
 	    }
-    
 	} elseif ($_POST['show'] == 'major') {
 		$query = 'select * from attendancelist Where major = \''.$_POST['bymajor'].'\'';
 		$result = mysqli_query($conn, $query) or die ('Failed to query '.mysqli_error($conn));
@@ -44,6 +48,14 @@
 			print "<h5>(".$row['course'].") on ".$row['coursedate']."</h5>";
 			print "</div>";
 		}
+	} elseif ($_POST['show'] == 'add') {
+		$name = $_POST['name']
+		$major = $_POST['major']
+		$course = $_POST['course']
+		$date = $_POST['cdate']
+		$attd = $_POST['attd']
+		$query = 'Insert Into attendancelist Values(\''.$_POST['name'].'\', \''.$_POST['major'].'\', \''.$_POST['course'].'\', \''.$_POST['cdate'].'\', \''.$_POST['attd'].'\')';
+		$result = mysqli_query($conn, $query) or die ('Failed to query '.mysqli_error($conn));
 	}
 	mysqli_free_result($result);
 	mysqli_close($conn);		
