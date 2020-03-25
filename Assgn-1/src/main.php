c7609d0109313d2c831273a15f6cce6d4407bf9c
atibrewal98 <ankit@siplnet.com>
2020-03-25 20:08:41 +0530

    Fix Main Page UI

--- Assgn-1/src/main.php
+++ Assgn-1/src/main.php
@@ -4,9 +4,93 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Book Store</title>
         <link rel= "stylesheet" type= "text/css"  href= "Assets/styles.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     </head>
-
     <body>
-        <h1>Main Page</h1>
+        <div class = "center">
+            <input type = "text" class = "searchbox" id = "sbar" placeholder = "Keyword(s)">
+            <input type = "button" class = "btn1" id = "sbtn" value = "Search">
+        </div>
+
+        <div class = "btns">
+            <span class = "txt" id = "sgn_in" onclick= "window.location.href = 'login.php'">Sign In</span>
+            <span class = "txt" id = "rgstr" onclick= "window.location.href = 'createAcc.php'">Register</span>
+            <input type = "button" class = "btn1" id = "cart" value = "Cart">
+            <sup class = "cVal" id = "cartVal">0</sup>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="column left">
+                <h1>Category</h1>
+                <ul id = "cList">
+                    <li><a>Storybook</a></li>
+                    <li><a>Contemporary Fiction</a></li>
+                    <li><a>Picture Book</a></li>
+                    <li><a>History</a></li>
+                </ul>
+            </div>
+
+            <div class="column right">
+                <div>
+                    <a class = "hLink" href = "main.php">Home</a>
+                    <span class = "txt1" id = "hSep"> > </span> 
+                    <a class = "hLink" id = "sLink" href = "">Storybook</a>
+
+                    <h1 id = "pHeading">All Books</h1>
+
+                    <input type = "button" class = "btn1 aRight" id = "sLow" value = "Sort by Price (Lowest)">
+                </div>
+                <br>
+                <div id = "entries" class = "mt20">
+                    <div class="card">
+                        <img src="Assets/images/book_1.jpeg" alt="Denim Jeans" style="width:100%">
+                        <h1>Tailored Jeans</h1>
+                        <p class="price">$19.99</p>
+                        <p>Some text about the jeans..</p>
+                        <button>Add to Cart</button>
+                    </div>
+
+                    <div class="card">
+                        <img src="Assets/images/book_2.jpeg" alt="Denim Jeans" style="width:100%">
+                        <h1>Tailored Jeans</h1>
+                        <p class="price">$19.99</p>
+                        <p>Some text about the jeans..</p>
+                        <button>Add to Cart</button>
+                    </div>
+
+                    <div class="card">
+                        <img src="Assets/images/book_3.jpeg" alt="Denim Jeans" style="width:100%">
+                        <h1>Tailored Jeans</h1>
+                        <p class="price">$19.99</p>
+                        <p>Some text about the jeans..</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script type = "text/javascript">
+            window.onload = function() {
+                document.getElementById("hSep").style.visibility = "hidden";
+                document.getElementById("sLink").style.visibility = "hidden";
+            }
+            
+            document.getElementById("sbtn").onclick = function () {
+                document.getElementById("pHeading").innerHTML = "Searching Resuts";
+            }
+
+            document.getElementById("sLow").onclick = function () {
+                document.getElementById("pHeading").innerHTML = "All Books (Sort by Price Lowest)";
+            }
+
+            $("#cList li").click(function() {
+                document.getElementById("pHeading").innerHTML = "All " + $(this).text();
+                document.getElementById("hSep").style.visibility = "visible";
+                document.getElementById("sLink").innerHTML = $(this).text();
+                document.getElementById("sLink").style.visibility = "visible";
+            })
+        </script>
     </body>
 </html>
