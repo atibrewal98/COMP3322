9ff4339c823626c3d01d9f3ca646beb0ca86a9b1
atibrewal98 <ankit@siplnet.com>
2020-04-21 12:19:07 +0530

    Last Change

--- Midterm-2/getmark.php
+++ Midterm-2/getmark.php
@@ -1,16 +1,16 @@
 <?php 
     $conn = mysqli_connect('sophia.cs.hku.hk', 'c1111a', 'sudoku', 'c1111a') or  http_response_code(405);;
 
-    $query = 'select * from gradebook';
+    $query = 'select stdName, stdNumber, assign1, assign2, midterm, exam from gradebook';
     $result = mysqli_query($conn, $query) or http_response_code(405);
 
     if (mysqli_num_rows($result) > 0) { 
-        $json_arr = array();
+        $records = array();
 
         while($row = mysqli_fetch_array($result)) {
-            $json_arr[] = array("stdName" => $row['stdName'], "stdNumber" => $row['stdNumber'], "assign1" => $row['assign1'], "assign2" => $row['assign2'], "midterm" => $row['midterm'], "exam" => $row['exam']);
+            $records[] = array("stdName" => $row['stdName'], "stdNumber" => $row['stdNumber'], "assign1" => $row['assign1'], "assign2" => $row['assign2'], "midterm" => $row['midterm'], "exam" => $row['exam']);
         }
 
-        echo json_encode($json_arr);
+        echo json_encode($records);
     }
  ?>
